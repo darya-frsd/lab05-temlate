e23344c6791df4900bdb50c8c2233247a6752680
darya-frsd <90180904+darya-frsd@users.noreply.github.com>
2022-04-09 16:45:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,7 @@
 #include "lab05-templates.h"
-#include "lab05-templates.cpp"
+
 
 int main() {
-	int a;
-	queue q(10);
-	while (std::cin >> a) {
-		try {
-			q.push(a);
-			std::cout << "\n";
-		}
-		catch (std::exception& e) {
-			std::cout << e.what() << "\n";
-		}
-	}
+	auto a1 = min_my(std::string("a"), std::string(100, 'x'));
+	assert(a1 == "a");
 }
