cff71c6e09eae1a03a101ed7eaf71faeca73556e
darya-frsd <90180904+darya-frsd@users.noreply.github.com>
2022-04-09 16:45:50 +0300

    Update lab05-templates.h

--- lab05-templates.h
+++ lab05-templates.h
@@ -1,65 +1,65 @@
-﻿// lab05-templates.h : включаемый файл для стандартных системных включаемых файлов
+// lab05-templates.h : включаемый файл для стандартных системных включаемых файлов
 // или включаемые файлы для конкретного проекта.
 
 #pragma once
 
 #include <utility>
 #include <queue>
+#include <cassert>
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 template<typename T>
-struct foo {
-	template<typename U>
-	struct minimum {
-		static int my_min(int a, int b);
-	};
+T min_my(const T& a, const T& b) {
+	if (a < b) {
+		a;
+	}
+	else {
+		return  b;
+	}
+}
 
-	template <typename F>
-	struct queue {
-	private:
-		T* uk;
-		const int size;
-		int top;
-	public:
-		queue<F>::queue(int maxSize) : size(maxSize)
-		{
-			uk = new F[size];
-			top = 0;
-		}
-		void push(const F&);
-		void pop();
-	};
-
-	/*template<typename F>
-	struct queue {
-	private:
-		std::queue<F> digits;
-		int N;
-	public:
-		queue(int m) {
-			digits.push(m);
-			N = m;
-		}
-
-		friend queue& operator>>(queue& value, F i) {
-			if (value.digits.size() < 2) {
-				value.digits.push(i);
-				return value;
-			}
-			else {
-				return value;
-			}
-		}
-
-		friend queue& operator<<(queue& value, F i) {
-			if (value.digits.top()) {
-				i = value.digits.front();
-				value.digits.pop();
-				return value;
-			}
-			else {
-				return value;
-			}
-		}
-	};*/
-
+template <typename F>
+struct queue {
+private:
+	F* uk;
+	const int size;
+	int top;
+public:
+	queue<F>::queue(int maxSize) : size(maxSize)
+	{
+		uk = new F[size];
+		top = 0;
+	}
+	void push(const F&);
+	void pop();
 };
+
+struct queue_overflow : std::runtime_error {
+	queue_overflow()
+		: std::runtime_error("the queue is full: ") {}
+};
+
+struct empty_queue : std::runtime_error {
+	empty_queue()
+		: std::runtime_error("the queue is empty: ") {}
+};
+
+template <typename F>
+void queue<F>::push(const F& value) {
+	if (top < size) {
+		throw queue_overflow();
+	}
+	uk[top++] = value;
+}
+
+template <typename F>
+void queue<F>::pop() {
+	if (top == 0) {
+		throw empty_queue();
+	}
+	uk[top--];
+}
